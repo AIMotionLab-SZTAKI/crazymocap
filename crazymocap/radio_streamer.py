28322cf2c8a72dce71014ba1916fe234eb919a46
Bodlaire <bodabenedek03@gmail.com>
2024-01-10 12:07:01 +0100

    updated radio_streamer

--- crazymocap/radio_streamer.py
+++ crazymocap/radio_streamer.py
@@ -6,11 +6,17 @@ import time
 import array
 from typing import List
 import numpy as np
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ip",  default= "192.168.2.141")
+parser.add_argument("-o", "--object_name",  default= "JoeBush1")
+parser.add_argument("-d", "--devid", default = 0)
+args = vars(parser.parse_args())
 
 
 def quat_2_yaw(quat: List[float]):
-    """ Takes a quaternion as a list in order of x, y, z, w, and returns its yaw.
-     TODO: Note what convention yaw is in, OR decide it doesn't matter in case of the car (it probably doesn't)"""
     x = quat[0]
     y = quat[1]
     z = quat[2]
@@ -22,51 +28,47 @@ def quat_2_yaw(quat: List[float]):
 
 
 class RadioStreamer:
-    """Class to encapsulate  the streaming crazyradio.
-    In theory, the only public method you should need is send_pose, and maybe close"""
-    def __init__(self, obj_name, devid, ip="192.168.2.141", mode=Crazyradio.MODE_PTX, channel=100, data_rate=Crazyradio.DR_250KPS):
-        # initialize radio
-        self.radio = Crazyradio(devid=devid)  # if you only have 1 dongle in your PC, this must be 0
+
+    def __init__(self, devid, ip=args['ip'], mode=Crazyradio.MODE_PTX, channel=100, data_rate=Crazyradio.DR_250KPS):
+        self.radio = Crazyradio(devid=devid)
         self.radio.set_channel(channel)
         self.radio.set_data_rate(data_rate)
         self.radio.set_mode(mode)
-        # make sure radio gets released before the program quits
         atexit.register(self.close)
         self.mocap = motioncapture.MotionCaptureOptitrack(ip)
-        self.obj_name = obj_name
+        self.obj_name = args['object_name']
         self.start_time = time.time()
 
-    def _timestamp(self):
+    def timestamp(self):
         return time.time() - self.start_time
 
-    def _wait_frame_parse_pose(self):
-        """ Returns None if the object isn't in frame. Else returns an  array.array (NOT LIST) like so:
-        [time_since_initialization, heading(radians), x(m), y(m), z(m)], where each element is a float
-        Before returning this data, it has to wait for an optitrack frame, which
-        can only happen with a maximum of 120 fps, so this function has a built-in frame limit"""
+    def wait_frame_parse_pose(self):
         self.mocap.waitForNextFrame()
         try:
             obj = self.mocap.rigidBodies[self.obj_name]
             yaw = quat_2_yaw([obj.rotation.x, obj.rotation.y, obj.rotation.z, obj.rotation.w])
-            return array.array("f", [self._timestamp()] + [yaw] + list(obj.position))
+            return array.array("f", [self.timestamp()] + [yaw] + list(obj.position))
         except KeyError:  # not in frame
             return None
 
+    def _send(self, data):
+        res = self.radio.send_packet(data)
+        #TODO: check if we got response?
+        if res is not None and res.ack:
+            print(f"sent data {data}, got response")
+        return res
+
     def send_pose(self):
-        pose = self._wait_frame_parse_pose()
+        pose = self.wait_frame_parse_pose()
         if pose is not None:
-            res = self.radio.send_packet(pose)
-            if res is not None and res.ack:
-                print(f"sent {pose}")
-            return res
+            self._send(pose)
 
     def close(self):
         self.radio.close()
 
-
 if __name__=="__main__":
     print("Starting TX")
-    streamer = RadioStreamer(obj_name="cf2", devid=1)
+    streamer = RadioStreamer(devid=args["devid"])
     try:
         while True:
             streamer.send_pose()
